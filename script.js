fcd06f9c24ab6d870bf182323f69ec1c434de70f
saurin thakkar <saurin.thakkar13@gmail.com>
2021-08-24 19:21:55 +0530

    add index file and script.js file

---
+++ script.js
@@ -0,0 +1,66 @@
+// var a = 'a';
+
+// if(a){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+
+// var a = undefined;
+
+// if(a > 0){
+//     console.log("Positive")
+// }
+// else if(a<0){
+//     console.log("Negative")
+// }
+// else{
+//     console.log("Zero number")
+// }
+
+/*---Loops---*/
+
+// var i =1;
+
+// // while(i <=5){
+// //     console.log(i)
+// //     i++;
+
+// // }
+
+// do{
+//     console.log(i)
+//     i++
+// }
+// while(i<=5)
+
+// var i,j;
+
+// for(i =0,j=10 ; i<10,j<100;){
+//     console.log(i)
+// }
+
+// var a;
+
+// console.log(typeof(a));
+
+
+/*---Function basics*/
+
+// function showAlert(a,b,c){
+//     console.log(a+b*c)
+
+// }
+
+// var c = showAlert(2,3)
+// console.log(c)
+
+/*---Variable Length Arguments*/
+
+function sum(a,b){
+    return a+b
+}
+
+var result = sum(4,5,9)
+console.log(result)
