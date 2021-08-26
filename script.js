2282c55c2eba8af0ffee3d4d306e659498f51478
saurin thakkar <saurin.thakkar13@gmail.com>
2021-08-26 14:52:59 +0530

    adding javascript examples

--- script.js
+++ script.js
@@ -58,9 +58,94 @@
 
 /*---Variable Length Arguments*/
 
-function sum(a,b){
-    return a+b
-}
+// function sum(a,b){
+//     return a+b
+// }
 
-var result = sum(4,5,9)
-console.log(result)
+// var result = sum(4,5,9)
+// console.log(result)
+
+/*---Function Hoisting*/
+
+// function hoistDemo() {
+//     console.log(i);
+//     var i=10;
+// }
+
+// hoistDemo();
+
+/**---Variable hoisting */
+
+// console.log(j);
+
+// function hoistDemo() {
+//     console.log(i);
+//     var i=10;
+// }
+
+// // hoistDemo();
+
+// var j = 100;
+
+/*---Functions and scope*/
+
+// var nameVar = "global"
+
+// function scopeDemo(){
+//     var nameVar = "function";
+//     console.log(nameVar)
+// }
+
+// scopeDemo()
+// console.log(nameVar);
+
+/*2nd Example*/ 
+
+// function c(){
+//     console.log("Inside c")
+// }
+
+// function b(){
+//     c()
+//     console.log("Inside b")
+// }
+// function a(){
+//     b();
+//     console.log("Inside a")
+// }
+
+// a()
+// console.log("global context")
+
+/*---Function within Function*/
+
+// function outer(){
+//     var outerVar = 10;
+//     function inner(){
+//         var innerVar = 100;
+//         console.log(outerVar)
+//     }
+
+//     inner()
+//     console.log(innerVar)
+// }
+
+// // inner();
+// outer();
+
+/*2nd example for function within function*/
+
+// var nameVar = "global"
+
+// function outerVar(){
+//     var nameVar = "outer";
+//     function inner(){
+//         var nameVar = "inner";
+//         console.log(nameVar);
+//     }
+//     inner()
+//     console.log(nameVar)
+// }
+
+// outerVar()
+// console.log(nameVar)
